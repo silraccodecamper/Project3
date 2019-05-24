6c77be3de5d09329c9bd3133fc092bc5b0473aa6
silraccodecamper <>
2019-05-24 08:32:49 -0700

    add the files

--- Project3/ViewController.swift
+++ Project3/ViewController.swift
@@ -15,6 +15,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var displayTask: UILabel!
 
+    @IBAction func futureTask(_ sender: UIButton) {
+        let prompts = ["What do you want to do 1 year from now",
+                       "Dream holiday to visit in next few months ",
+                       "Where do you want to see yourself in next year",
+                       "whats one thing you want to change in near future",
+                       "where do you want to celebrate your next birthday",
+                       "what do you want to do if you get a break next week",
+                       "what do you want to see in your job couple of months from now",]
+        
+        let random = prompts[Int(arc4random_uniform(UInt32(prompts.count)))]
+        displayTask.text = random
+        
+    }
+
+    @IBAction func pastTask(_ sender: UIButton) {
+        let prompts = ["Describe your day 10 days ago",
+                       "Tell about your holiday last year",
+                       "Best memories from prevous trip ",
+                       "Talk about any thing in the past that inspired you",
+                       "Movie you watched 10 days ago","Whats the wackiest gift you received recently",
+                       "Explain the embarassing moments you experienced couple of days ago"]
+        
+        let random = prompts[Int(arc4random_uniform(UInt32(prompts.count)))]
+        displayTask.text = random
+    }
+    
+    @IBAction func nextTurn(_ sender: UIButton) {
+    displayTask.text = String(Int.random(in: 1 ... 10))
+    print("next turn")
+    }
 }
 
